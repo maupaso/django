bdaabedf653f427be3ec127d762321a09c8453e9
Marcos Aurélio De Paiva Souza <marcossouza@br.ibm.com>
2018-11-02 22:06:38 -0300

    Adicionando caminho para as views

--- carioquinha/views.py
+++ carioquinha/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-	return HttpResponse("HEEEELLLLO")
+	    template = loader.get_template('carioquinha/index.html')
+	    context = {
+	    	'ola': "OI"
+	    }
+
+	    return render(request, 'carioquinha/index.html')
